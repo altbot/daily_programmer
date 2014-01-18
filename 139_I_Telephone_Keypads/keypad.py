04f6e43e0b9191ba59e3ae7b8883477a089d0ab3
altbot <matthew.ault@gmail.com>
2014-01-18 18:56:51 +0000

    139_I_Telephone Keypads: Initial Challenge++ implementation

---
+++ 139_I_Telephone_Keypads/keypad.py
@@ -0,0 +1,35 @@
+# Daily programer challenge 139 - Telephone Keypads - Intermediate
+# http://www.reddit.com/r/dailyprogrammer/comments/1sody4/12113_challenge_139_intermediate_telephone_keypads/
+
+import re, sys
+
+numMap = {
+"2": "abc",
+"3": "def",
+"4": "ghi",
+"5": "jkl",
+"6": "mno",
+"7": "pqrs",
+"8": "tuv",
+"9": "wxyz",
+}
+
+nums = [val[0] for val in sys.argv[1:]] # Doing Challenge++, discard frequency info
+
+words = open('/usr/share/dict/words').read().lower().rstrip()
+charsList = []
+for num in nums:
+	try:
+		charsList.append(numMap[num])
+	except Exception:
+		pass # ignore 0s and 1s
+
+# Build-up regex pattern
+pattern = ""
+for charset in charsList:
+	pattern += "[%s]" % charset
+pattern += "\w*"
+
+# Retrieve unique matches
+matches = set(re.findall(pattern, words))
+print sorted(matches, key=len)
